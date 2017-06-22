73aa01513f6a3ed1e7dce2ad39fc634c6435a0d9
h-hub <harsha.kj89@gmail.com>
2017-06-23 02:05:00 +0800

    register module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MdDatepickerModule, MdNativeDateModule, MdDialogModule, MdButtonModule } from '@angular/material';
 import { NgModule } from '@angular/core';
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { AlertService, UserService } from '../../services/services';
+import { User } from '../../models/user';
 
 @Component({
     moduleId: module.id,
@@ -9,26 +11,50 @@ import { AlertService, UserService } from '../../services/services';
 })
 
 export class RegisterComponent {
-    model: any = {};
-    loading = false;
+  userForm: FormGroup;
+  user = User;
 
-    constructor(
-        private router: Router,
-        private userService: UserService,
-        private alertService: AlertService) { }
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
 
-    register() {
-        this.loading = true;
-        this.userService.create(this.model)
-            .subscribe(
-                data => {
-                    // set success message and pass true paramater to persist the message after redirecting to the login page
-                    this.alertService.success('Registration successful', true);
-                    this.router.navigate(['/login']);
-                },
-                error => {
-                    this.alertService.error(error);
-                    this.loading = false;
-                });
-    }
+  createForm() {
+    this.userForm = this.fb.group({ // <-- the parent FormGroup
+      username: ['', Validators.required ],
+      password: '',
+      passwordRepeate: '',
+      email: '',
+      emailRepeate: ''
+    });
+  }
+
+  onSubmit(){
+      
+  }
 }
+
+
+// export class RegisterComponent {
+//     model: any = {};
+//     loading = false;
+
+//     constructor(
+//         private router: Router,
+//         private userService: UserService,
+//         private alertService: AlertService) { }
+
+//     register() {
+//         this.loading = true;
+//         this.userService.create(this.model)
+//             .subscribe(
+//                 data => {
+//                     // set success message and pass true paramater to persist the message after redirecting to the login page
+//                     this.alertService.success('Registration successful', true);
+//                     this.router.navigate(['/login']);
+//                 },
+//                 error => {
+//                     this.alertService.error(error);
+//                     this.loading = false;
+//                 });
+//     }
+// }
