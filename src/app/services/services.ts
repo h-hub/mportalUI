4882a070589949ec924498d851f48c5644ee81a1
h-hub <harsha.kj89@gmail.com>
2017-06-28 00:58:32 +0800

    config service changes

--- src/app/services/services.ts
+++ src/app/services/services.ts
@@ -1,9 +1,10 @@
 /**
  * Created by bjayamanna on 6/16/2017.
  */
+export * from './config.service';
 export * from './alert.service';
 export * from './authentication.service';
 export * from './user.service';
 export * from './dialog.service';
 export * from './session.service';
-export * from './config.service';
+
